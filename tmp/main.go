dfd4c2db56e9583432ff5f6f8230ae24dbf3f002
Saeed <saeedk@systemgroup.net>
2023-08-01 16:21:55 +0330

    dd

--- tmp/main.go
+++ tmp/main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
-	"unicode/utf8"
 )
 
 func main() {
-	s := "Hello, 世界 A یی"
-	fmt.Println(len(s))                    // "13"
-	fmt.Println(utf8.RuneCountInString(s)) // "9"
-	for i := 0; i < len(s); {
-		r, size := utf8.DecodeRuneInString(s[i:])
-		fmt.Printf("%d\t%c\t%d\n", i, r, size)
-		i += size
-	}
+	r := [...]int{3, 5: 3, 2, 3: 1}
+	fmt.Println(r)
 
 }
