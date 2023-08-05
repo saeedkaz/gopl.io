18ab34b13d7a649d5fcde9d73240ef522e632d87
saeedkaz <saeed.kazemy@gmail.com>
2023-08-06 00:23:33 +0330

    4.2

--- tmp/main.go
+++ tmp/main.go
@@ -4,8 +4,15 @@ import (
 	"fmt"
 )
 
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
 func main() {
-	r := [...]int{3, 5: 3, 2, 3: 1}
-	fmt.Println(r)
+	s := []int{0, 1, 2, 3, 4, 5}
+	reverse(s)
+	fmt.Printf("%v\n", s)
 
 }
